98c5b419d22d8a50dcf7075844199600f6499e81
Soufiane-KS <soufianekousta1@gmail.com>
2025-01-21 13:28:35 +0100

    setting up an emulated shell

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -20,4 +20,32 @@ creds_handler.setFormatter(logging_format)
 creds_logger.addHandler(creds_handler)
 
 
+def emulated_shell(channel, client):
+    channel.send(b'corporate-jumpbox$ ')
+    command = b""
+    while True:
+        char = channel.recv(1)
+        channel.send(char)
+        if not char:
+            channel.close()
 
+        command += char
+
+        if char == b'\r':
+            if command.strip() == b'exit':
+                response = b'\n Goodbye!\n'
+                channel.close()
+            elif command.strip() == b'pwd':
+                response = b'\n' + b'\\usr\\local' + b'\r\n'
+            elif command.strip() == b'whoami':
+                response = b'\n' + b'Soufiane' + b'\r\n'
+            elif command.strip() == b'ls':
+                response = b'\n' + b'jumpbox1.conf' + b'\r\n'
+            elif command.strip() == b'cat jumpbox1.conf':
+                response = b'\n' +b'Go to testWebsite.com' + b'\r\n'
+            else:
+                response = b'\n' + bytes(command.strip()) +b'\r\n'
+
+        channel.send(response)
+        channel.send(b'corporate-jumpbox$ ')
+        command=b''
